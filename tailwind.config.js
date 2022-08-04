0988bd38c13594d0b965404169b903a7a0995bfc
farvellhub <farvellsmurf@gmail.com>
2022-08-04 23:27:46 +0200

    refactorized tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+  mode: "jit",
+  purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkmode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
