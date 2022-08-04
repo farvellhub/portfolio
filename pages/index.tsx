3676b9a11947644ddaca85799fba7bd3b474c7db
farvellhub <farvellsmurf@gmail.com>
2022-08-04 22:58:44 +0200

    added tailwind and postcss configs. deleted some stuff from vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
 import { NextPage } from "next";
 
-import { Slideshow } from "../components";
+// import { Slideshow } from "../components";
 
 
 const IndexPage: NextPage = () => {
     return (
         <>
-            <div>
-                <h1>
+            <div className="fade-out w-full flex justify-items-center flex-wrap">
+                <h1 className="text-6xl font-title w-full pb-2 uppercase font-bold bg-clip-text bg-gradient-to-br from-typescript to-pink-400 text-transparent menuMin:text-9xl">
                     Hi visitor!
                 </h1>
-                <p>
-                    I&apos;m <span>javascript</span> and <span>typescript</span> developer focused on website design and frontend development.
+                <p className="text-xl w-full pb-2 text-whippy menuMin:text-2xl">
+                    I&apos;m <span className="text-yellow-600">javascript</span> and <span className="text-typescript">typescript</span> developer focused on website design and frontend development.
                 </p>
                
-                <p>Below you can see some of my projects.</p>
+                <p className="text-xl w-full pb-16 menuMin:text-2xl">Below you can see some of my projects.</p>
             </div>
 
-            <div>
-                <Slideshow />
+            <div className="py-20 px-10 bg-typescript rounded-sm">
+                {/* <Slideshow /> */}
             </div>
         </>
     );
